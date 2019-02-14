e1949833a93272eb4e4d3eed3c33095b22e10d33
František Konečný <frantisek.konecny@mhm.cz>
2019-02-14 16:40:07 +0100

    bugfix - mapping type

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -99,6 +99,7 @@ var budgetController = (function() {
         },
 
         deleteItem: function(type, id) {
+            debugger;
             var index, ids;
             ids = data.allItems[type].map(function(current) {
                 return current.id;
@@ -175,10 +176,10 @@ var UIController = (function() {
             // create HTML string with placeholder text
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
-                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
-                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+                html = '<div class="item clearfix" id="exp-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
             }
 
             //replace placeholder text
@@ -287,7 +288,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     var ctrlDeleteItem = function(event) {
         var itemID, splitID, type, ID;
-        itemID = (event.target.parentNode.parentNode.parentNode.parentNode.id);
+        itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
 
         if (itemID) {
             //inc-0
