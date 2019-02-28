d2878f2ed1336f119c9e6103238d2e201095908d
František Konečný <frantisek.konecny@mhm.cz>
2019-02-28 14:56:43 +0100

    rewritten code chall 8 from the master od JS

--- 7-ES6/starter/script.js
+++ 7-ES6/starter/script.js
@@ -599,7 +599,7 @@ johnAtlete6.calculateAge();
 /******************************** */
 // Lecture: Coding challange 8
 /******************************** */
-
+/* 
 class Town {
   constructor(name, buildYear) {
     this.name = name;
@@ -745,3 +745,99 @@ function displayReport(town) {
 }
 
 displayReport(town);
+ */
+
+/******************************** */
+// Lecture: Coding challange 8 = solution from lecture
+/******************************** */
+//Data model
+class Element {
+  constructor(name, buildYear) {
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+}
+
+class Park extends Element {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTrees = numTrees;
+  }
+
+  treeDensity() {
+    const density = this.numTrees / this.area;
+    console.log(
+      `${this.name} has a tree density of ${density} trees per square km.`
+    );
+  }
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear);
+    this.length = length;
+    this.size = size;
+  }
+
+  classifyStreet() {
+    const classification = new Map();
+    classification.set(1, "tiny");
+    classification.set(2, "small");
+    classification.set(3, "normal");
+    classification.set(4, "big");
+    classification.set(5, "huge");
+
+    console.log(
+      `${this.name}, build in ${this.buildYear}, is a ${classification.get(
+        this.size
+      )} street.`
+    );
+  }
+}
+
+//Data
+
+const allParks = [
+  new Park("Green Park", 2001, 0.2, 215),
+  new Park("National Park", 1984, 2.9, 3541),
+  new Park("Oak Park", 1953, 0.4, 949)
+];
+const allStreets = [
+  new Street("Ocean Avenue", 1999, 1.1, 4),
+  new Street("Evergreen Street", 2008, 2.7, 2),
+  new Street("4th Street", 2015, 0.8),
+  new Street("Sunset Blvd", 1992, 2.5, 5)
+];
+
+function calc(arr) {
+  const sum = arr.reduce((prev, cur, index) => prev + cur, 0);
+  return [sum, sum / arr.length];
+}
+
+function reportParks(p) {
+  console.log("---------PARKS REPORT-----------");
+  //Density
+  p.forEach(element => element.treeDensity());
+  //average age
+
+  const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+  const [totalAge, avgAge] = calc(ages);
+  console.log(`Our ${p.length} parks have an average of ${avgAge} years`);
+
+  //which park have more than 1000 trees
+  const i = p.map(el => el.numTrees).findIndex(el => el >= 1000);
+  console.log(`${p[i].name} has more than 1000 trees.`);
+}
+
+function reportStreets(s) {
+  console.log("---------STREETS REPORT-----------");
+  const [totalLength, avgLength] = calc(s.map(el => el.length));
+
+  console.log(`Our ${s.length} streets have a total length of ${totalLength}km, with and average of ${avgLength}`);
+
+  s.forEach(el => el.classifyStreet());
+}
+
+reportParks(allParks);
+reportStreets(allStreets);
