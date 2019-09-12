c4e577c86e45baac2fa7ffb0a1e283e25fb72c47
jenjenmuj <35293569+jenjenmuj@users.noreply.github.com>
2019-09-12 15:07:58 +0200

    Update index.js

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,10 @@
 // Global app controller
+import axios from 'axios';
+
+
+async function getResults(query) {
+     console.log('do change 222');
+}
+
+getResults();
+//ef74bbc8c246f2bb6b6f5c5aa7ec31a5
