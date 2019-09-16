a5691f46985f0724d59b50e3bde2d786a86fc124
frantisek.konecny <fr.konecny@gmail.com>
2019-09-16 10:49:45 +0200

    limit titles

--- 9-forkify/starter/src/js/views/searchView.js
+++ 9-forkify/starter/src/js/views/searchView.js
@@ -2,6 +2,33 @@ import {elements} from './base';
 
 export const getInput = () => elements.searchInput.value;
 
+export const clearInput = () => {
+    elements.searchInput.value = '';
+};
+
+export const clearResults = () => {
+    elements.searchResList.innerHTML = '';
+}
+
+
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        //creates array with string split with space
+        //0 accumulater is by default
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        //return the resul , join is opposite of split
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
+
 const renderRecipe = recipe => {
     const markup = `
         <li>
@@ -10,7 +37,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
