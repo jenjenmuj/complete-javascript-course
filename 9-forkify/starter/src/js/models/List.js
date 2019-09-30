adf3fc46e457493293d4006b7f43ac37e73decc5
jenjenmuj <fr.konecny@gmail.com>
2019-09-30 13:14:08 +0200

    serving and ingredinets updating, now creating List

---
+++ 9-forkify/starter/src/js/models/List.js
@@ -0,0 +1,20 @@
+import uniqid from 'uniqid';
+
+export default class List{
+    constructor() {
+        this.items = [];
+    }
+
+    addItem (count, unit, ingredient) {
+        const item = {
+            id: uniqid(),
+            count,
+            unit,
+            ingredient
+        }
+    }
+
+    deleteItem (id) {
+        this.items.splice();
+    }
+}
